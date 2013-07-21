2189b9e1abf896d22cedba24914478df6cb4a0e8
Ikuo Terado <eqobar@gmail.com>
2013-07-22 02:50:47 +0900

    Changed memo list to use webdis

--- app/scripts/controllers/memo-list.js
+++ app/scripts/controllers/memo-list.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular.module('memoApp')
-  .controller('MemoListCtrl', function ($scope, Memo) {
-    $scope.memos = Memo.query();
+  .controller('MemoListCtrl', function ($scope, $http) {
+    $http.get('//192.168.33.10:7379/GET/memos').success(function (data) {
+      $scope.memos = data.GET || [];
+    });
+
     $scope.orderProp = 'age';
+
+    $scope.createMemo = function () {
+      $scope.memos.push({
+        key: 'memo_' + $scope.memos.length
+      });
+    };
   });

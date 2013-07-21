1f78f4ce6bb4b0ba068240305fd3022ab5af3195
Ikuo Terado <eqobar@gmail.com>
2013-07-22 02:54:07 +0900

    Changed memo detail to use webdis

--- app/scripts/controllers/memo-detail.js
+++ app/scripts/controllers/memo-detail.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('memoApp')
-  .controller('MemoDetailCtrl', function ($scope, $routeParams, Memo) {
+  .controller('MemoDetailCtrl', function ($scope, $routeParams, $http) {
     $scope.memoId = $routeParams.memoId;
-    $scope.memo = Memo.get({memoId: $routeParams.memoId});
+
+    $http.get('//192.168.33.10:7379/GET/' + $scope.memoId).success(function (data) {
+      $scope.memo = data.GET || {
+        title: '',
+        content: ''
+      };
+    });
   });

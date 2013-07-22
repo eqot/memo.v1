fb11ede8c78093af603f854497ffad55e5c05298
Ikuo Terado <eqobar@gmail.com>
2013-07-22 23:07:54 +0900

    Supported saving memo to server

--- app/scripts/controllers/memo-detail.js
+++ app/scripts/controllers/memo-detail.js
@@ -4,15 +4,20 @@ angular.module('memoApp')
   .controller('MemoDetailCtrl', function ($scope, $routeParams, $http) {
     $scope.memoId = $routeParams.memoId;
 
+    $scope.saved = true;
+
     $http.get('//192.168.33.10:7379/GET/' + $scope.memoId).success(function (data) {
-      $scope.memo = data.GET || {
-        title: '',
-        content: ''
+      var memo = JSON.parse(data.GET);
+      $scope.memo = {
+        title: memo.title || '',
+        content: memo.content || ''
       };
     });
 
     var updateTimer = null;
     $scope.update = function () {
+      $scope.saved = false;
+
       if (updateTimer) {
         clearTimeout(updateTimer);
         updateTimer = null;
@@ -24,11 +29,15 @@ angular.module('memoApp')
     };
 
     function save () {
-      console.log('save()');
       var memo = {
         title: $scope.memo.title,
         content: $scope.memo.content
       };
-      console.log(memo);
+      var jsonData = JSON.stringify(memo);
+      // console.log(jsonData);
+      $http.get('//192.168.33.10:7379/SET/' + $scope.memoId + '/' + jsonData).success(function () {
+        // console.log('saved.');
+        $scope.saved = true;
+      });
     }
   });
